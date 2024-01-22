e85bb674260b10020994b176a5d875258efa39ee
naveenkumar <157213733+JammasulaNaveenKumar@users.noreply.github.com>
2024-01-22 11:07:59 +0530

    Add files via upload

---
+++ javaSCRIPT SMALL/day1.js
@@ -0,0 +1,68 @@
+
+// function fetchRandomJoke(f){
+// f("Hai ra mama")
+// }
+// function translateJoke(a,f){
+// let joke=a;
+// f(joke)
+// }
+// function postJoke(a,f){
+// f(a)
+// }
+// fetchRandomJoke((joke) => {
+//     console.log(joke);
+
+//     translateJoke(joke, (translatedJoke) => {
+//         console.log(translatedJoke);
+
+//         postJoke(translatedJoke, (aaaa) => {
+//             console.log(aaaa);
+//         });
+//     });
+// });
+
+
+// function f1(a,f){
+// setTimeout(()=>{
+//     f(a)
+// },8000)
+
+// }
+// function f2(b,f){
+
+// f(b)
+
+// }
+// function f3(c){
+// console.log(c);
+// }
+
+// f1("bye bye babu",(a1)=>{console.log(a1)
+//     f2(a1,(b1)=>{console.log(b1);
+//     f3(b1)
+//     })
+//     ;})
+
+// function f1(){
+//     let a=10;
+//     let b=1
+//     f2(a,b)
+// }
+
+// function f2(p1,p2){
+//     let res
+//     setTimeout(()=>{
+//         res=p1+p2+1000
+//         f3(res)
+//     },5000)
+   
+// }
+
+// function f3(res1){
+// console.log(res1);
+// }
+// f1()
+
+
+
+
---
+++ javaSCRIPT SMALL/day2.js
@@ -0,0 +1,11 @@
+
+class Person1 extends Person{
+    constructor(){
+        this.name="naveen";
+        this.age=25;
+        super()
+    }
+   
+}
+let a=Person1();
+a.details()
